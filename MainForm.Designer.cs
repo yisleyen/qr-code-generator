3059a62a25e8e76f4905e70e23971c00930acb19
Yusuf İşleyen <isleyenyusuf@gmail.com>
2015-02-04 21:07:20 +0200

    Updated

---
+++ MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace QRCode
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.pcQrCode = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcQrCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(16, 39);
+            this.txtValue.Multiline = true;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(250, 125);
+            this.txtValue.TabIndex = 0;
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(13, 13);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(113, 13);
+            this.lblValue.TabIndex = 1;
+            this.lblValue.Text = "Please enter the value";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.Location = new System.Drawing.Point(191, 8);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.BtnCreateClick);
+            // 
+            // pcQrCode
+            // 
+            this.pcQrCode.Location = new System.Drawing.Point(272, 39);
+            this.pcQrCode.Name = "pcQrCode";
+            this.pcQrCode.Size = new System.Drawing.Size(125, 125);
+            this.pcQrCode.TabIndex = 3;
+            this.pcQrCode.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Location = new System.Drawing.Point(272, 8);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSaveClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 176);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pcQrCode);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.txtValue);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Code Generator";
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pcQrCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.PictureBox pcQrCode;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+
+namespace QRCode
+{
+    public partial class MainForm : Form
+    {
+        private readonly Random _rng = new Random();
+        private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreateClick(object sender, EventArgs e)
+        {
+            pcQrCode.Image = CreateQrCode(txtValue.Text);
+            btnSave.Visible = true;
+        }
+
+        private static Image CreateQrCode(string value)
+        {
+            var qe = new QRCodeEncoder
+                         {
+                             QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE,
+                             QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.L,
+                             QRCodeVersion = 1
+                         };
+
+            var btmp = qe.Encode(value);
+            return btmp;
+        }
+
+        private void BtnSaveClick(object sender, EventArgs e)
+        {
+            var img = pcQrCode.Image;
+
+            img.Save(Application.StartupPath + "\\" + RandomString(10) + ".png");
+            MessageBox.Show(string.Format("QR Code has been saved"));
+        }
+
+        private string RandomString(int size)
+        {
+            var buffer = new char[size];
+
+            for (var i = 0; i < size; i++)
+            {
+                buffer[i] = Chars[_rng.Next(Chars.Length)];
+            }
+            return new string(buffer);
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            btnSave.Visible = false;
+        }
+    }
+}
